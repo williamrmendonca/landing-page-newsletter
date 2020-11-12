4f275317db63c6534133b2fecd2f9fbb3ee66071
williamrmendonca <william@tecmasternet.com.br>
2020-11-12 10:58:32 -0300

    Change Tech add AOS Animation

--- src/components/SectionTech/index.tsx
+++ src/components/SectionTech/index.tsx
@@ -7,12 +7,12 @@ import icons from './content'
 import * as S from './styles'
 
 const SectionTech = () => (
-  <S.Wrapper>
+  <S.Wrapper data-aos="fade-up" data-aos-delay="900">
     <Container>
       <Heading reverseColor>Tecnologias utilizadas</Heading>
       <S.IconsContainer>
         {icons.map(({ name, image }) => (
-          <S.Icon key={name}>
+          <S.Icon key={name} data-aos="flip-left" data-aos-delay="900">
             <S.Icons src={`img/tech/${image}`} alt={name} loading="lazy" />
             <S.IconsName>{name}</S.IconsName>
           </S.Icon>
